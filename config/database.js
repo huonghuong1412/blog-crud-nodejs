9a51fd43dc5e1700f28ebb203e7de0b9b121b23b
= <daigiahuong9999@gmail.com>
2020-11-16 15:12:26 +0700

    fix deploy

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
+const DB_URL = process.env.DATABASE_URL || "mongodb://localhost/blog_nodejs";
+
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost/blog_nodejs', {
+        await mongoose.connect(DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
